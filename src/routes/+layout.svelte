5fc6863f199281077bcca633263180d094ac9840
Seunguk Lee <sulee04@kaist.ac.kr>
2025-08-16 23:17:08 +0900

    feat: auth backbone

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,19 @@
-<script lang="ts">
-	import favicon from '$lib/assets/favicon.svg';
+<script>
+  import { invalidate } from '$app/navigation'
+  import { onMount } from 'svelte'
 
-	let { children } = $props();
+  let { data, children } = $props()
+  let { session, supabase } = $derived(data)
+
+  onMount(() => {
+    const { data } = supabase.auth.onAuthStateChange((_, newSession) => {
+      if (newSession?.expires_at !== session?.expires_at) {
+        invalidate('supabase:auth')
+      }
+    })
+
+    return () => data.subscription.unsubscribe()
+  })
 </script>
 
-<svelte:head>
-	<link rel="icon" href={favicon} />
-</svelte:head>
-
-{@render children?.()}
+{@render children()}
