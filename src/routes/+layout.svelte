146536568403874df7808af5fccfa5a9124902aa
Seunguk Lee <sulee04@kaist.ac.kr>
2025-08-29 12:56:06 +0900

    feat: add manage routes

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,19 +1,20 @@
 <script>
-  import { invalidate } from '$app/navigation'
-  import { onMount } from 'svelte'
+	import { invalidate } from '$app/navigation';
+	import ThemeProvider from '$lib/components/ThemeProvider.svelte';
+	import { onMount } from 'svelte';
 
-  let { data, children } = $props()
-  let { session, supabase } = $derived(data)
+	let { data, children } = $props();
+	let { session, supabase } = $derived(data);
 
-  onMount(() => {
-    const { data } = supabase.auth.onAuthStateChange((_, newSession) => {
-      if (newSession?.expires_at !== session?.expires_at) {
-        invalidate('supabase:auth')
-      }
-    })
+	onMount(() => {
+		const { data } = supabase.auth.onAuthStateChange((_, newSession) => {
+			if (newSession?.expires_at !== session?.expires_at) {
+				invalidate('supabase:auth');
+			}
+		});
 
-    return () => data.subscription.unsubscribe()
-  })
+		return () => data.subscription.unsubscribe();
+	});
 </script>
 
-{@render children()}
+<ThemeProvider>{@render children()}</ThemeProvider>
