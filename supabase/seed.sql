1a5e4199660183b815334ab0f2e482bf92441eb2
Seunguk Lee <sulee04@kaist.ac.kr>
2025-09-16 17:32:04 +0900

    test(db): change seed for testing

--- supabase/seed.sql
+++ supabase/seed.sql
@@ -6,7 +6,7 @@
 -- insiders@grida.co / password
 INSERT INTO auth.users ( instance_id, id, aud, role, email, encrypted_password, email_confirmed_at, recovery_sent_at, last_sign_in_at, raw_app_meta_data, raw_user_meta_data, created_at, updated_at, confirmation_token, email_change, email_change_token_new, recovery_token) 
 VALUES 
-  ('00000000-0000-0000-0000-000000000000', uuid_generate_v4(), 'authenticated', 'authenticated', 'modernpromenader@gmail.com', crypt('password', gen_salt('bf')), current_timestamp, current_timestamp, current_timestamp, '{"provider":"email","providers":["email"]}', '{}', current_timestamp, current_timestamp, '', '', '', '');
+  ('00000000-0000-0000-0000-000000000000', uuid_generate_v4(), 'authenticated', 'authenticated', 'modernpromenader@gmail.com', crypt('password0993', gen_salt('bf')), current_timestamp, current_timestamp, current_timestamp, '{"provider":"email","providers":["email"]}', '{}', current_timestamp, current_timestamp, '', '', '', '');
 
 
 -- test user email identity
